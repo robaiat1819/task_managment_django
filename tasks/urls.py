4379c8aaaf1fb81a2b860a5e64d9e6ec71ca3504
Robaiat Hossain Fardin <fardinrobaiat@gmail.com>
2025-11-02 23:58:07 +0600

    Initial Project Done

---
+++ tasks/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from tasks.views import show_task
+
+
+urlpatterns = [path("show_task/", show_task)]
---
+++ tasks/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    # Work With database
+    # Transform Data
+    # Data Pass
+    # HTTP Response / json Response
+    return HttpResponse("Welcome to the Task Managment System")
+
+
+def contact(request):
+    return HttpResponse("<h1 style='color: yellow'>This Is Contact Page<h/>")
+
+
+def show_task(request):
+    return HttpResponse("This Is a Task Page")
